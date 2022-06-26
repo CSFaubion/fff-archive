fa2eeff39cd8acc67bcdc32dcf917ea32c7ef4d1
CSFaubion <cameronfaubion@gmail.com>
2022-06-26 18:42:12 -0500

    refactor owner data

--- patch/myleague.py
+++ patch/myleague.py
@@ -1,6 +1,6 @@
 from espn_api.football import League
 
-# this patch is intended to add some data from espn that the espn-api 
+# this patch is intended to add some data from espn that the espn-api
 # package doesn't keep.
 
 
@@ -8,19 +8,38 @@ class MyLeague(League):
     def __init__(self, league_id: int, year: int, espn_s2=None, swid=None, username=None, password=None, debug=False):
         super().__init__(league_id=league_id, year=year, espn_s2=espn_s2,
                          swid=swid, username=username, password=password, debug=debug)
+        self.get_owner_data()
 
-        # TODO: add primary owner ids for teams
+        # TODO: get weekly rosters
+
+
+    def get_owner_data(self):
+
         espn_data = self.espn_request.league_get(params={'view': 'mTeam'})
-        teamdata = espn_data.get('teams', None)
-        if teamdata is None:
+
+         # add primary owner ids for teams
+        team_data = espn_data.get('teams', None)
+        if team_data is None:
             print('keyerror: failed to find key "teams"')
             exit(0)
-        # for each team, add the corresponding espn owner id from the espn data.
         for team in self.teams:
-            for espn_team in teamdata:
+            for espn_team in team_data:
                 if espn_team['id'] == team.team_id:
                     team.primary_owner = espn_team.get('primaryOwner', '')
 
-        # TODO: add member data
-
-        # TODO: get weekly rosters
+        # add member data            
+        member_data = espn_data.get('members', None)
+        if member_data is None:
+            print('keyerror: failed to find key "teams"')
+            exit(0)
+        for member in member_data:
+            for team in self.teams:
+                if team.primary_owner != member['id']:
+                    continue
+                else:
+                    team.member = {
+                        'display_name' : member['displayName'],
+                        'first_name' : member['firstName'],
+                        'espn_owner_id' : member['id'],
+                        'last_name' : member['lastName']
+                    }
