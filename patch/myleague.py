1121b029f054554db381d1825d3d87990b8474ae
CSFaubion <cameronfaubion@gmail.com>
2022-06-26 15:40:34 -0500

    add team primary owner ids

--- patch/myleague.py
+++ patch/myleague.py
@@ -1,15 +1,26 @@
 from espn_api.football import League
 
+# this patch is intended to add some data from espn that the espn-api 
+# package doesn't keep.
+
+
 class MyLeague(League):
     def __init__(self, league_id: int, year: int, espn_s2=None, swid=None, username=None, password=None, debug=False):
-        super().__init__(league_id=league_id, year=year, espn_s2=espn_s2, swid=swid, username=username, password=password, debug=debug)
+        super().__init__(league_id=league_id, year=year, espn_s2=espn_s2,
+                         swid=swid, username=username, password=password, debug=debug)
 
-        #TODO: add primary owner ids for teams
+        # TODO: add primary owner ids for teams
         espn_data = self.espn_request.league_get(params={'view': 'mTeam'})
         teamdata = espn_data.get('teams', None)
         if teamdata is None:
             print('keyerror: failed to find key "teams"')
             exit(0)
+        # for each team, add the corresponding espn owner id from the espn data.
+        for team in self.teams:
+            for espn_team in teamdata:
+                if espn_team['id'] == team.team_id:
+                    team.primary_owner = espn_team.get('primaryOwner', '')
 
+        # TODO: add member data
 
-        #TODO: get weekly rosters
+        # TODO: get weekly rosters
