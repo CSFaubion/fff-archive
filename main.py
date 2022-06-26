475b69370b982ed17e5c31baf81a14de19a38396
CSFaubion <cameronfaubion@gmail.com>
2022-06-26 13:21:45 -0500

    set up credentials and espn_api package stuff

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+
+from espn_api.football import League
+
+# this file is intended to be the entry point for this project. change the
+# target year and enter your credentials into credentials.json
+
+target_year = 2020
+
+if __name__ == "__main__":
+    # read credentials from file.
+    creds = {}
+    with open("credentials.json", "r") as credentials:
+        creds = json.load(credentials)
+
+    #create league object
+    league = League(
+        league_id=creds.get("leagueId", -1),
+        year=target_year,
+        espn_s2=creds.get("s2", ""),
+        swid=creds.get("swid", "")
+    )
+
