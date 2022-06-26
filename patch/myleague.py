af00f6fbf12ffb00628911afbf1532e65975d0b0
CSFaubion <cameronfaubion@gmail.com>
2022-06-26 15:33:20 -0500

    add team data request.

--- patch/myleague.py
+++ patch/myleague.py
@@ -3,4 +3,10 @@ from espn_api.football import League
 class MyLeague(League):
     def __init__(self, league_id: int, year: int, espn_s2=None, swid=None, username=None, password=None, debug=False):
         super().__init__(league_id=league_id, year=year, espn_s2=espn_s2, swid=swid, username=username, password=password, debug=debug)
+
+        #TODO: add primary owner ids for teams
+        espn_data = self.espn_request.league_get(params={'view': 'mTeam'})
+        teamdata = espn_data.get('teams', None)
         
+
+        #TODO: get weekly rosters
