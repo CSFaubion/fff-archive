ff67d130cfb23e29dac431476f4cd6b9340fe6e9
CSFaubion <cameronfaubion@gmail.com>
2022-07-04 11:44:41 -0500

    add roster key

--- patch/myleague.py
+++ patch/myleague.py
@@ -9,9 +9,7 @@ class MyLeague(League):
         super().__init__(league_id=league_id, year=year, espn_s2=espn_s2,
                          swid=swid, username=username, password=password, debug=debug)
         self.get_owner_data()
-
-        # TODO: get weekly rosters
-
+        self.get_weekly_rosters()
 
     def get_owner_data(self):
         '''This function adds primaryOwner IDs to teams, and adds a
@@ -19,7 +17,7 @@ class MyLeague(League):
         more than one member per team'''
         espn_data = self.espn_request.league_get(params={'view': 'mTeam'})
 
-         # add primary owner ids for teams
+        # add primary owner ids for teams
         team_data = espn_data.get('teams', None)
         if team_data is None:
             print('keyerror: failed to find key "teams"')
@@ -29,7 +27,7 @@ class MyLeague(League):
                 if espn_team['id'] == team.team_id:
                     team.primary_owner = espn_team.get('primaryOwner', '')
 
-        # add member data            
+        # add member data
         member_data = espn_data.get('members', None)
         if member_data is None:
             print('keyerror: failed to find key "teams"')
@@ -40,12 +38,12 @@ class MyLeague(League):
                     continue
                 else:
                     team.member = {
-                        'display_name' : member['displayName'],
-                        'first_name' : member['firstName'],
-                        'espn_owner_id' : member['id'],
-                        'last_name' : member['lastName']
+                        'display_name': member['displayName'],
+                        'first_name': member['firstName'],
+                        'espn_owner_id': member['id'],
+                        'last_name': member['lastName']
                     }
-    
+
     def get_weekly_rosters(self):
         '''Gets all of the weekly rosters for the season.
         ESPN changes the format when they archive older seasons.
@@ -53,3 +51,5 @@ class MyLeague(League):
         rosters for matchups, but no bench players.
         For non-archived seasons this function gets all players on 
         rosters and thier positions for each scoring period'''
+
+        roster_key = 'rosterForMatchupPeriod' if self.year <= 2017 else 'rosterForCurrentScoringPeriod'
