25264330bcca5b0bca4b548081240d1d7038a188
CSFaubion <cameronfaubion@gmail.com>
2022-07-24 18:35:54 -0500

    remove unused import

--- db/draftpick_dto.py
+++ db/draftpick_dto.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, create_engine
 from sqlalchemy.orm import relationship
 from base import Base
 
