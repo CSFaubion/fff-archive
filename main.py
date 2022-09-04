80e735a2fdf367a5e41a68427a7b0cb4413220bf
CSFaubion <cameronfaubion@gmail.com>
2022-09-04 11:34:56 -0500

    add db changes to main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from patch.myleague import MyLeague as League
+from db.db import add_league, Base
 
 # this file is intended to be the entry point for this project. change the
 # target year and enter your credentials into credentials.json
@@ -22,5 +25,14 @@ if __name__ == "__main__":
     )
 
 
-    with open("league2020.json", "w") as outfile:
-        json.dump(league.dump_league_data(), outfile, indent=2)
+    with open("league"+str(target_year)+".json", "w") as outfile:
+        data = json.dump(league.dump_league_data(), outfile, indent=2)
+
+    with open("league"+str(target_year)+".json", "r") as infile:
+        engine = create_engine("sqlite+pysqlite:///b.db", echo=False, future=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        add_league(session, data)
+    
